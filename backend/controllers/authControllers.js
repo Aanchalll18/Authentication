62680a2efd6ddda0144c310f846671f417473dd0
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-07 01:43:30 +0530

    commit

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,40 @@
+import User from "../models/userModel.js";
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+export const register=async(req,res)=>{
+    const {email,name,password}=req.body;
+    if(!email || !password || !name){
+        return res.json({
+            success:false,
+            message:"All fields are mondatory"
+        })
+    }
+    try {
+        const existingUser=await User.find({email});
+        if(existingUser){
+            return res.json({
+                success:false,
+                message:"User Already exists"
+            })
+        }
+       const hashedPassword=await bcrypt.hash(password,10);
+       const user =new User({name,email,password:hashedPassword})
+       await user.save()
+
+       const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'})
+
+       res.cookie('token',token,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === 'production',
+        sameSite:process.env.NODE_ENV==='production'?'None':'Strict',
+        maxAge:7*24*60*60*1000
+       })
+    } catch (error) {
+        console.log(error.message);
+        return res.json({
+            success:false,
+            message:error.message
+        })
+    }
+};
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -11,6 +11,6 @@ const userSchema=new mongoose.Schema({
     resetOtpExpireAt:{type:Number,default:0}
 })
 
-const userModel=mongoose.models.user || mongoose.model('user',userSchema);
+const User=mongoose.models.user || mongoose.model('user',userSchema);
 
-export default userModel;
+export default User;
