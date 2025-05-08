5909491ab1692ae3a4207402065a2bce5ebfa092
Aanchallll18 <kumariaachal002@gmail.com>
2025-05-08 17:53:06 +0530

    commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	const [state, setState] = useState("Sign Up");
+	return (
+		<div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
+			<img
+				src={assets.logo}
+				alt="logo"
+				className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
+			/>
 
-export default Login
+			<div>
+				<h2>{state === "Sign Up" ? "Create Account" : "Login"}</h2>
+				<p>
+					{state === "Sign Up"
+						? "Create your account"
+						: "Login to your account"}
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
